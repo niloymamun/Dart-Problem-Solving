a046ea6215d4c4fa3c43e7a38446cfd0f1832409
neloy-mamun <neloymamun06@gmail.com>
2023-09-08 02:01:07 +0600

    update

--- lib/01 Practice.dart
+++ lib/01 Practice.dart
@@ -3,32 +3,24 @@
  Print out a message that tells how many years
  they have to be 100 years old.*/
 import 'dart:io';
+
 void main() {
   while (true) {
     try {
       print("Enter Your Birth year");
       String? birthYear = stdin.readLineSync();
-      int currentYear = DateTime
-          .now()
-          .year;
+      int currentYear = DateTime.now().year;
       int convert = int.parse(birthYear!);
       int result = currentYear - convert;
       print("Your Current  Age is $result years old");
-      
-      if(result<=100){
-        int yearsToHundred =100-result; 
+
+      if (result <= 100) {
+        int yearsToHundred = 100 - result;
         print(" You have $yearsToHundred years to be 100");
       }
       break;
-
-  }catch(e){
-  print("Error message $e");
+    } catch (e) {
+      print("Error message $e");
+    }
   }
 }
-  
-
-
-
-
-}
-
