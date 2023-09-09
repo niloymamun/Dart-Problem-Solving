54cc0ae6bc1781326c803dfa6e0f84e76cc4b84a
AL MAMUN <143861941+niloymamun@users.noreply.github.com>
2023-09-09 16:04:38 +0600

    Create 02 Practice.dart

---
+++ lib/02 Practice.dart
@@ -0,0 +1,23 @@
+/* Question: Ask the user for a number. 
+ * Depending on whether the number is even or odd,
+ *  print out an appropriate message to the user.*/
+
+import 'dart:io';
+void main(){
+  while(true){
+    try{
+      print("Please Enter a Number");
+  int number = int.parse(stdin.readLineSync());
+      if(number%2==0){
+        print("This Number is $number Even");
+        
+      }else print("This Number is $number Odd");
+      
+    }catch(e){
+      print(e);
+    }
+    
+  }
+  
+  
+}
